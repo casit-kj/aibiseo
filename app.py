f209f751420f6745d373d109882208e47de6d061
jamesohell <jamesohe@casit.co.kr>
2024-02-20 15:53:46 +0900

    logger apply

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from module.app_config import parse_args
+from module.app_logging import get_logger, print_daemon_start
 
 def main(args):
-    pass
+    app_logger, err_logger, model_logger = get_logger(args.logdir)    
+    print_daemon_start(app_logger)
 
 if __name__ == '__main__':
     args = parse_args()
-    print(args)
     main(args)
