33b1faa8a2678b46ac9f95e263419822192100da
jamesohell <jamesohe@casit.co.kr>
2024-03-14 16:46:43 +0900

    3월 14일 오후 4시

---
+++ webapps/log_blueprint.py
@@ -0,0 +1,22 @@
+"""
+module.name : chat_list_blueprint.py
+module.purpose: Chat List bluepoint
+module.create.date: 2024. 02. 21
+module.writer: Haengun Oh
+module.writer.email: jamesohe@gmail.com
+"""
+
+from flask import Blueprint, request, jsonify, Flask, render_template, session
+from libs.mods.mnglogger import LoggingManager
+from libs.mods.dbsource import DBSource
+
+
+class ChatListBlueprint:
+    def __init__(self, loggerManager, dbServer):  
+        self.loggerManager = loggerManager
+        self.dbServer = dbServer
+        
+    def chatList(self):
+        message, code = self.dbServer.chatlist()
+        return jsonify({'result_Data': message,'status':code,'loginconfirm': session.get('logged_in')})
+   
