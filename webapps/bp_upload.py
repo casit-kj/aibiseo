eafd2b078d902aebe0ac1cd0a4c55f698cdc8f79
aicasit <jamesohe@casit.co.kr>
2024-03-24 15:02:23 +0900

    change aibiseo.json

---
+++ webapps/bp_upload.py
@@ -0,0 +1,17 @@
+"""
+module.name : index_blueprint.py
+module.purpose: Index bluepoint
+module.create.date: 2024. 02. 21
+module.writer: Haengun Oh
+module.writer.email: jamesohe@gmail.com
+"""
+
+from flask import Blueprint, request, jsonify, Flask, render_template
+from libs.mods.mnglogger import LoggingManager
+import chromadb
+class UsermanageBlueprint:
+    def __init__(self, loggerManager):  
+        self.loggerManager = loggerManager
+        
+    def usermanage(self):
+        return render_template('usermanage.html')
